623ca88c5ac14440fc4b2010d68d8135df9d9cc8
Dat Pham <dpham@paramountcommerce.com>
2023-01-12 10:38:03 +0700

    init project

---
+++ src/main/java/com/graphql/demo/silver/GraphqlSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.graphql.demo.silver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graphql/demo/silver/GraphqlSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphql.demo.silver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
