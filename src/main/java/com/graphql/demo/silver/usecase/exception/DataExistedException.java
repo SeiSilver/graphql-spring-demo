1306d07e66d6ab97c7f7e2c129e0c072508cb33a
Dat Pham <dpham@paramountcommerce.com>
2023-01-16 11:01:36 +0700

    add exception for query

---
+++ src/main/java/com/graphql/demo/silver/usecase/exception/DataExistedException.java
@@ -0,0 +1,9 @@
+package com.graphql.demo.silver.usecase.exception;
+
+public class DataExistedException extends Exception {
+
+  public DataExistedException(String message) {
+    super(message);
+  }
+
+}
---
+++ src/main/java/com/graphql/demo/silver/usecase/exception/DataNotFoundException.java
@@ -0,0 +1,9 @@
+package com.graphql.demo.silver.usecase.exception;
+
+public class DataNotFoundException extends Exception {
+
+  public DataNotFoundException(String message) {
+    super(message);
+  }
+
+}
